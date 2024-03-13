5be894a38b8de483f0633c537b85420d8b6aad36
RogerJ <827090192@qq.com>
2024-03-13 18:16:06 +0800

    修改axios请求拦截器配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 // 创建axios实例，多个实例互不影响
-const instance = axios.create({
+const request = axios.create({
   baseURL: 'http://cba.itlike.com/public/index.php?s=/api/',
   timeout: 2000
 })
 
-instance.interceptors.request.use(config => {
+request.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
-  if (token) {
+  if (typeof token !== 'undefined') {
     config.headers.token = token
   }
   return config
@@ -16,10 +16,10 @@ instance.interceptors.request.use(config => {
   return Promise.reject(error)
 })
 
-instance.interceptors.response.use(response => {
+request.interceptors.response.use(response => {
   return response.data
 }, error => {
   return Promise.reject(error)
 })
 
-export default instance
+export default request
