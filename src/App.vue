2753bd6d5410be81a377905881fc91c2c0583765
RogerJ <827090192@qq.com>
2024-03-15 18:25:17 +0800

    初始化时，将本地的用户信息保存到vuex中

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@
 </template>
 
 <script>
+import { getLocalItem } from '@/utils/storage'
+import { mapMutations } from 'vuex'
 
+export default {
+  created () {
+    // 页面初始化时，从本地查找userInfo，放入vuex/user中
+    const userObj = getLocalItem('userInfo')
+    if (userObj) {
+      this.setUserInfo(userObj)
+    }
+  },
+  methods: {
+    ...mapMutations('user', ['setUserInfo'])
+  }
+}
 </script>
 
 <style lang="less">
