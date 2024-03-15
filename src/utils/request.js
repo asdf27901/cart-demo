7ccb47f1dcf5d1cbde08374ab686b2c5061ad9d1
RogerJ <827090192@qq.com>
2024-03-15 15:30:48 +0800

    不对响应状态码为200做处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { Toast } from 'vant'
 
 // 创建axios实例，多个实例互不影响
 const request = axios.create({
@@ -18,14 +17,6 @@ request.interceptors.request.use(config => {
 })
 
 request.interceptors.response.use(response => {
-  const { status, message } = response.data
-  if (status !== 200) {
-    Toast.fail({
-      message,
-      forbidClick: true
-    })
-    return Promise.reject(message)
-  }
   return response.data
 }, error => {
   return Promise.reject(error)
