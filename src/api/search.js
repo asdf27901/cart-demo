1bb66218e07674d9d63ccc30f317780ed5318ee2
RogerJ <827090192@qq.com>
2024-03-20 18:12:35 +0800

    修改商品搜索接口为对象传入

--- src/api/search.js
+++ src/api/search.js
@@ -1,11 +1,9 @@
 import request from '@/utils/request'
 
-export const searchGood = async (goodName) => {
+export const searchGood = (obj) => {
   return request({
     url: '/goods/list',
     method: 'get',
-    params: {
-      goodsName: goodName
-    }
+    params: obj
   })
 }
