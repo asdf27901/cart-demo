e0a30c1622b98de9202fd9a89fcd14a7df42813f
RogerJ <827090192@qq.com>
2024-03-22 16:04:20 +0800

    添加axios重试机制

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,10 @@ import { Toast } from 'vant'
 // 创建axios实例，多个实例互不影响
 const request = axios.create({
   baseURL: 'http://cba.itlike.com/public/index.php?s=/api/',
-  timeout: 2000
+  timeout: 2000,
+  _retryCount: 0,
+  retryTime: 3,
+  retryDelay: 1000
 })
 
 request.interceptors.request.use(config => {
@@ -30,8 +33,30 @@ request.interceptors.response.use(response => {
   console.log(response.data)
   Toast.clear(true) // 关闭所有弹窗，主要是loading弹窗
   return response.data
-}, error => {
-  return Promise.reject(error)
+}, async error => {
+  const config = error.config
+  // 如果没有配置对象，直接返回异常
+  if (!config) {
+    return Promise.reject(error)
+  }
+  // 当重试次数大于配置次数，直接返回异常
+  if (config._retryCount >= config.retryTime) {
+    Toast('重试失败，请刷新页面')
+    return Promise.reject(error)
+  }
+  Toast.fail({
+    message: '正在重试...请稍后',
+    forbidClick: true
+  })
+  // 以下代码开始请求重试
+  config._retryCount++
+  console.log(config._retryCount)
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, config.retryDelay)
+  })
+  return request(config)
 })
 
 export default request
