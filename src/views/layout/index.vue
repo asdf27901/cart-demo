41f94125e2ddca9d549a81ed465382d4816354f1
RogerJ <827090192@qq.com>
2025-01-21 00:10:55 +0800

    修改导航栏跳转问题

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -13,10 +13,10 @@
 
     <!--标签栏切换-->
     <van-tabbar v-model="active" route>
-      <van-tabbar-item icon="wap-home" to="/home">首页</van-tabbar-item>
-      <van-tabbar-item icon="list-switching" to="/sort">分类</van-tabbar-item>
-      <van-tabbar-item icon="shopping-cart" to="/cart">购物车</van-tabbar-item>
-      <van-tabbar-item icon="contact" to="/my">我的</van-tabbar-item>
+      <van-tabbar-item icon="wap-home" to="/home" replace>首页</van-tabbar-item>
+      <van-tabbar-item icon="list-switching" to="/sort" replace>分类</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart" to="/cart" replace>购物车</van-tabbar-item>
+      <van-tabbar-item icon="contact" to="/my" replace>我的</van-tabbar-item>
     </van-tabbar>
   </div>
 
